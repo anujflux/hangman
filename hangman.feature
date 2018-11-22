30435dbb89bd0d635bf28f3e3696bac2b91c2bab
Anuj Sharma <anuj@fluxfederation.com>
2018-11-22 16:54:59 +1300

    Added more logic to the game. CAn accept user input, validate the guess and show whether it is correct or not. Also added a spec file but it's irrelevant to the game at the moment, will probably start adding specs. Feature file is very generic at the moment, not sure if I will use it in the future.

---
+++ hangman.feature
@@ -0,0 +1,23 @@
+Feature: A game of hangman
+
+  Scenario: Start a new game of hangman
+
+  Scenario: Indication of how many letters there are in the word that are to be guessed
+
+  Scenario: I want to submit one letter guesses and be told whether my guess is either in the word, not in the word, or already guessed
+
+  Scenario: I want the game to handle bad input (like numbers, symbols, and letters I've previously guessed) gracefully and with an appropriate error message so that I am not punished unduly for mistakes
+
+  Scenario: I want to see the letters I've guessed correctly revealed on the hidden word that I am trying to guess so that I can guess the word more readily
+
+  Scenario: I want to see the letters I've guessed incorrectly so that I don't guess the same wrong letter again
+
+  Scenario: I would like to see how many lives are remaining
+
+  Scenario: there should be 1 life deducted every time I make an incorrect guess of a letter that I have not already guessed
+
+  Scenario: I want the game to end when I have run out of lives
+
+  Scenario: I want the game to end when I have guessed the full word
+
+  Scenario: When I lose the game I want to see the word I was trying to guess
--- hangman.rb
+++ hangman.rb
@@ -3,46 +3,68 @@ require 'pry'
 $stdout.sync = true
 
 class Hangman
-  @word_list
+  WORD_LIST = ['hangman']
 
 
   def initialize
-    @word_list = ["Virat Kohli", "Sachin Tendulkar"]
-    @letters_guessed = Array.new
-    @correct_letters = Array.new
+    @letters_guessed = []
   end
 
   def select_word
-    @current_word = @word_list[Random.rand(@word_list.length)]
+    @current_word = WORD_LIST.sample
+  end
+
+  def display_dashes
     @current_word.split.each do |part_of_word|
-      part_of_word.split('').each do |letter|
-        @correct_letters.push(letter)
+        print "_" * part_of_word.length
+        print " "
+      end
+  end
+
+  def get_user_input
+    while letter = gets.chomp
+      case letter
+      when "exit"
+        exit
+      else
+        validate_guess(letter) unless letter_already_guessed?(letter)
       end
     end
-     @correct_letters
   end
 
-
-  def user_input
-    letter = nil
-    p "Guess a letter"
-    letter = gets.chomp
-  end
-
-  def display
-    @current_word.split.each do |part_of_word|
-      print "_" * part_of_word.length
-      print " "
+  def validate_guess(letter)
+    @letters_guessed.push(letter)
+    if is_guess_correct_or_wrong?(letter)
+      puts "Correct guess"
+    else
+      puts "Incorrect guess, please try again"
     end
   end
 
-  ## TODO: Refactor based on catchup
+  def letter_already_guessed?(letter)
+    if @letters_guessed.include?(letter)
+      puts "Letter guessed before as well, please select another letter"
+      puts "Here are all the letters you have already guessed: "
+      puts @letters_guessed
+      return true
+    else
+      return false
+    end
+  end
+
+  def is_guess_correct_or_wrong?(letter)
+    return @current_word.include?(letter)
+  end
+
+
+  ## DONE: Refactor based on catchup
   ## DONE: Display to the user how long the word is
-  ## TODO: Ask the user to select a letter
-  ## TODO: Identify whether the letter has already been used or whether the letter is part of the word or not
+  ## DONE: Ask the user to select a letter
+  ## DONE: Identify whether the letter has already been used or whether the letter is part of the word or not
 end
 
 new_game = Hangman.new
 new_game.select_word
-
-new_game.display
+new_game.display_dashes
+puts "\nGuess a letter\n"
+new_game.get_user_input
