540655982f20b4a82255edf403e24ab4ef52a8c2
Anuj Sharma <anuj@fluxfederation.com>
2018-12-04 22:15:16 +1300

    more refactoring, more DONE and TODO

--- hangman.rb
+++ hangman.rb
@@ -35,27 +35,46 @@ class Hangman
     end
   end
 
-  def get_user_guess
-    puts "\nGuess a letter\n"
-    while lives_remaining > 0 && letter = gets.chomp do
-        case letter
-        when "exit"
+  def show_message_and_continue(message)
+    puts message
+  end
+
+  def start_game
+    while user_input = get_user_input do
+      if user_input == "exit"
+        abort_game "Bye, see you next time !"
+      elsif user_input.length > 1
+        show_message_and_continue "You can only guess one letter at a time"
+      else
+        check_lives_remaining
+        validate_guess(user_input) unless letter_already_guessed?(user_input)
+        if has_won?
+          binding.pry
+          show_message_and_continue "You have won ! See you next time !"
           exit
-        else
-          validate_guess(letter) unless letter_already_guessed?(letter)
         end
-    end unless has_won?
+      end
+    end
+  end
+
+  def get_user_input
+    puts "\nGuess a letter\n"
+    gets.chomp
   end
 
   def validate_guess(letter)
     @letters_guessed.push(letter)
     if is_guess_correct_or_wrong?(letter)
-      puts "Correct guess"
+      show_message_and_continue "Correct guess"
     else
-      puts "Incorrect guess, please try again"
-      @lives_lost = @lives_lost + 1
+      life_lost
+      show_message_and_continue "Incorrect guess, please try again. You now have #{lives_remaining} lives left"
     end
     display_dashes_with_correct_guesses
+  end
+
+  def life_lost
+    @lives_lost = @lives_lost + 1
   end
 
   def letter_already_guessed?(letter)
@@ -71,21 +90,33 @@ class Hangman
     return @current_word.include?(letter)
   end
 
-  def start_game
+  def load_game
     select_word
     display_welcome_message
     display_dashes
-    get_user_guess
+    start_game
   end
 
   def display_welcome_message
-    puts "Welcome to Hangman!!"
-    puts "You are guessing a word of #{@current_word.length} letters"
-    puts "Type exit at anytime to quit the game"
+    show_message_and_continue "Welcome to Hangman!!"
+    show_message_and_continue "You are guessing a word of #{@current_word.length} letters"
+    show_message_and_continue "You have #{MAX_LIVES} guesses"
+    show_message_and_continue "Type exit at anytime to quit the game"
+  end
+
+  def check_lives_remaining
+    if lives_remaining <= 1
+      abort_game("You do not have enought lives left to continue the game")
+    end
   end
 
   def lives_remaining
     MAX_LIVES - @lives_lost
+  end
+
+  def abort_game(reason)
+    puts reason
+    exit
   end
 
   def has_won?
@@ -94,7 +125,7 @@ class Hangman
 end
 
 new_game = Hangman.new
-new_game.start_game
+new_game.load_game
 
   ## DONE: Refactor based on catchup
   ## DONE: Display to the user how long the word is
@@ -104,7 +135,8 @@ new_game.start_game
 
   ## DONE: Display the dashes and correct guessed words after every letter guessed
   ## TODO: Let the user know whether when they win
-  ## TODO: Add maximum guesses allowed feature
+  ## TODO: Show the correct word when the user loses the game
+  ## DONE: Add maximum guesses allowed feature
 
   ## LATER: Write some rspecs
   ## LATER: Restart a game
